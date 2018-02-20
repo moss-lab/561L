cb4ecefc051365023e4368cecddc86d8530e8487
rjandr <randrews@iastate.edu>
2018-02-19 21:07:56 -0600

    Added reverse strand function

--- z_out_to_bw.py
+++ z_out_to_bw.py
@@ -22,6 +22,7 @@ fMFE_wig = pyBigWig.open('fMFE_'+filename+'.bw', 'w')
 chromosome = str(input("What chromsome is your gene on? (e.g., chr1): "))
 genomic_start = int(input("What is the starting coordinate of your gene sequence? (e.g., 2555639; without commas): " ))
 step_size = int(input("What is the step size for your data? (e.g. 1): " ))
+strand = input("What strand is your gene on (+ or -)?: ")
 
 MFE_wig.addHeader([("chr1",	248956422),("chr2", 242193529),("chr3", 198295559),("chr4", 190214555),("chr5", 181538259),("chr6", 170805979),("chr7", 159345973),("chr8", 145138636),("chr9",138394717),("chr10",133797422),("chr10",133797422),("chr11",135086622)])
 zscore_wig.addHeader([("chr1",	248956422),("chr2", 242193529),("chr3", 198295559),("chr4", 190214555),("chr5", 181538259),("chr6", 170805979),("chr7", 159345973),("chr8", 145138636),("chr9",138394717),("chr10",133797422),("chr10",133797422),("chr11",135086622)])
@@ -38,16 +39,33 @@ ED_list = []
 fMFE_list = []
 
 with open(filename, 'r') as f:
-    lines = f.readlines()[1:]
-    for row in lines:
-        data = row.split('\t') # this splits each row based on "tab
-        print(len(data))
-        if (len(data) == 14) and ('\-' in data[5]):
-                print(data)
-                data.remove(data[5])
+    if strand == "+":
+        lines = f.readlines()[1:]
+        for row in lines:
+            data = row.split('\t') # this splits each row based on "tab
+            print(len(data))
+            if (len(data) == 14) and ('\-' in data[5]):
+                    print(data)
+                    data.remove(data[5])
 
-                #corrected_row = ('\t'.join(data))
-                print(data)
+                    #corrected_row = ('\t'.join(data))
+                    print(data)
+                    #print(row)
+                    i = data[0]
+                    j = data[1]
+                    genomic_end = int(genomic_start)+int(step_size)
+                    MFE = float(data[2])
+                    MFE_list.append(MFE)
+                    zscore = float(data[3])
+                    zscore_list.append(zscore)
+                    pscore = float(data[4])
+                    pscore_list.append(pscore)
+                    ED = float(data[5])
+                    ED_list.append(ED)
+                    fMFE = float(data[6])
+                    fMFE_list.append(fMFE)
+
+            if len(data) == 14:
                 #print(row)
                 i = data[0]
                 j = data[1]
@@ -63,23 +81,46 @@ with open(filename, 'r') as f:
                 fMFE = float(data[6])
                 fMFE_list.append(fMFE)
 
-        if len(data) == 14:
-            #print(row)
-            i = data[0]
-            j = data[1]
-            genomic_end = int(genomic_start)+int(step_size)
-            MFE = float(data[2])
-            MFE_list.append(MFE)
-            zscore = float(data[3])
-            zscore_list.append(zscore)
-            pscore = float(data[4])
-            pscore_list.append(pscore)
-            ED = float(data[5])
-            ED_list.append(ED)
-            fMFE = float(data[6])
-            fMFE_list.append(fMFE)
+    if strand == "-":
+        lines = reversed(open(filename).readlines()[1:])
+        for row in lines:
+                data = row.split('\t') # this splits each row based on "tab
+                print(len(data))
+                if (len(data) == 14) and ('\-' in data[5]):
+                        print(data)
+                        data.remove(data[5])
+                        #corrected_row = ('\t'.join(data))
+                        print(data)
+                        #print(row)
+                        i = data[0]
+                        j = data[1]
+                        genomic_end = int(genomic_start)+int(step_size)
+                        MFE = float(data[2])
+                        MFE_list.append(MFE)
+                        zscore = float(data[3])
+                        zscore_list.append(zscore)
+                        pscore = float(data[4])
+                        pscore_list.append(pscore)
+                        ED = float(data[5])
+                        ED_list.append(ED)
+                        fMFE = float(data[6])
+                        fMFE_list.append(fMFE)
 
-
+                if len(data) == 14:
+                    #print(row)
+                    i = data[0]
+                    j = data[1]
+                    genomic_end = int(genomic_start)+int(step_size)
+                    MFE = float(data[2])
+                    MFE_list.append(MFE)
+                    zscore = float(data[3])
+                    zscore_list.append(zscore)
+                    pscore = float(data[4])
+                    pscore_list.append(pscore)
+                    ED = float(data[5])
+                    ED_list.append(ED)
+                    fMFE = float(data[6])
+                    fMFE_list.append(fMFE)
 
 
 #print(MFE_list)
